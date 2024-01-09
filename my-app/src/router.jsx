f84526b4fe3c739e3128971d45a2f2a2ac2ec9e8
admin <admin@admins-MacBook-Pro-6.local>
2024-01-09 23:14:10 +0900

    merge

--- my-app/src/router.jsx
+++ my-app/src/router.jsx
@@ -10,17 +10,14 @@ import Login from './pages/Login'
 import Map from './pages/Map'
 import RouteWalk from "./components/RouteWalk";
 import React from "react";
-<<<<<<< HEAD
 import NavTime from "./pages/NavTime"
 import NavDest from "./pages/NavDest"
 import SignupNickname from "./pages/SignupNickname";
 import SignupInterest from "./pages/SignupInterest";
 import Home from "./pages/Home"
 import NavTheme from "./pages/NavTheme";
-=======
 import EndCourse from "./pages/EndCourse";
 import ExitGuide from "./components/ExitGuide";
->>>>>>> 9bd9ecfe75ca2a92ed9fd71c440f020ebb6f6a0a
 
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -31,18 +28,15 @@ import ExitGuide from "./components/ExitGuide";
         <Route path="/navtime" element={<NavTime/>}/> 
         <Route path="/map" element={<Map/>} />      
         <Route path="*" element={<h1>NotFound</h1>} />
-<<<<<<< HEAD
         <Route path="/home" element={<Home/>}/>
         <Route path="/route" element={<RouteWalk/>}/>
         <Route path="/navdest" element={<NavDest/>}/>
         <Route path="/navtheme" element={<NavTheme/>}/>
         <Route path="/signupnickname" element={<SignupNickname/>}/>
         <Route path="/signupinterest" element={<SignupInterest/>}/>
-=======
         <Route path="/route" element={<RouteWalk/>} />
         <Route path="/endcourse" element={<EndCourse/>} />
         <Route path="/eg" element={<ExitGuide/>} />
->>>>>>> 9bd9ecfe75ca2a92ed9fd71c440f020ebb6f6a0a
       </Route>
     )
   );
