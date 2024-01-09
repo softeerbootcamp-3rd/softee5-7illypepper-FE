c525e7cf7a86be6c1ba53e127446e9fad927544d
admin <admin@adminui-MacBookPro-7.local>
2024-01-09 18:10:09 +0900

    complete EndCourse

--- my-app/src/pages/EndCourse.js
+++ my-app/src/pages/EndCourse.js
@@ -5,6 +5,19 @@ import { useForm, FormProvider, Controller } from "react-hook-form";
 import React, {useState} from 'react';
 import ActivatedFeedback from "../components/ActivatedFeedback";
 import DeactivatedFeedback from "../components/DeactivatedFeedback";
+import actStar from "../assets/actStar.png";
+import deactStar from "../assets/deactStar.png";
+
+import crCourse from "../assets/courseReview/courseReview_course.png";
+import crPic from "../assets/courseReview/courseReview_picture.png";
+import crPicAct from "../assets/courseReview/courseReview_pic_act.png";
+import crDog from "../assets/courseReview/courseReview_dog.png";
+import crTrend from "../assets/courseReview/courseReview_trend.png";
+import crMood from "../assets/courseReview/courseReview_mood.png";
+import crView from "../assets/courseReview/courseReview_view.png";
+import crCalm from "../assets/courseReview/courseReview_calm.png";
+import crBright from "../assets/courseReview/courseReview_bright.png";
+import crBaby from "../assets/courseReview/courseReview_baby.png";
 
 const EndCourse = () => {
     const navigate = useNavigate();
@@ -13,6 +26,12 @@ const EndCourse = () => {
     const id = watch("id");
 
     const [rating, setRating] = useState(0);
+
+    const [selCrPic, setSelCrPic] = useState(false);
+
+    const onClickCrPic = () => {
+        setSelCrPic(!selCrPic);
+    }
 
     const handleStarClick = (index) => {
         setRating(index);
@@ -39,29 +58,153 @@ const EndCourse = () => {
                     </div>
                 </div>
 
-                <div style={{display: 'flex', position: 'absolute', top: '209px', left: '20px'}}>
+                <div style={{
+                    display: 'flex',
+                    position: 'absolute',
+                    top: '224px',
+                    left: '76px'
+                }}>
                     {[1, 2, 3, 4, 5].map((index) => (
-                        <span key={index} onClick={() => handleStarClick(index)}>
-                            {index <= rating ? '★' : '☆'}
-                        </span>
+                        <img
+                            key={index}
+                            src={index <= rating ? actStar : deactStar}
+                            onClick={() => handleStarClick(index)}
+                            alt={`star-${index}`}
+                            style={{
+                                cursor: 'pointer',
+                                marginRight: index < 5 ? '2px' : '0px',
+                                width: '40px',
+                                height: '40px',
+                                flexShrink: 0
+                            }}
+                        />
                     ))}
                 </div>
 
+                <div style={{
+                    position: "absolute",
+                    top: '330px',
+                    left: '22px',
+                    color: selCrPic ? 'var(--main_text, #FF6D3F)' : 'var(--gray_500, #949494)',
+                    fontFamily: 'Pretendard',
+                    fontSize: '14.292px',
+                    fontStyle: 'normal',
+                    fontWeight: 600,
+                    lineHeight: 'normal'
+                }}>
+                    {selCrPic ? "1/5" : "0/5"}
+                </div>
+
+                <div style={{
+                    position: "absolute",
+                    top: '360px',
+                    left: '20px'
+                }}>
+                    <div style={{
+                        marginBottom: '14px'
+                    }}>
+                        <img
+                            src={crCourse}
+                            style={{
+                                width: '161px',
+                                height: '34px',
+                                marginRight: '8px'
+                            }}
+                        />
+                        <img
+                            src={selCrPic ? crPicAct : crPic}
+                            onClick={() => onClickCrPic()}
+                            style={{
+                                width: '140px',
+                                height: '34px'
+                            }}
+                        />
+                    </div>
+                    <div style={{
+                        marginBottom: '14px'
+                    }}>
+                        <img
+                            src={crDog}
+                            style={{
+                                width: '201px',
+                                height: '34px',
+                                marginRight: '8px'
+                            }}
+                        />
+                        <img
+                            src={crTrend}
+                            style={{
+                                width: '109px',
+                                height: '34px'
+                            }}
+                        />
+                    </div>
+                    <div style={{
+                        marginBottom: '14px'
+                    }}>
+                        <img
+                            src={crMood}
+                            style={{
+                                width: '176px',
+                                height: '34px',
+                                marginRight: '8px'
+                            }}
+                        />
+                        <img
+                            src={crView}
+                            style={{
+                                width: '137px',
+                                height: '34px'
+                            }}
+                        />
+                    </div>
+                    <div style={{
+                        marginBottom: '14px'
+                    }}>
+                        <img
+                            src={crCalm}
+                            style={{
+                                width: '149px',
+                                height: '34px',
+                                marginRight: '8px'
+                            }}
+                        />
+                        <img
+                            src={crBright}
+                            style={{
+                                width: '125px',
+                                height: '34px'
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <img
+                            src={crBaby}
+                            style={{
+                                width: '176px',
+                                height: '34px',
+                                marginRight: '8px'
+                            }}
+                        />
+                    </div>
+                </div>
+
                 {
-                    rating === 0 ?
-                        <div style={{position: 'absolute', top: '712px'}}>
-                            <DeactivatedFeedback/>
-                        </div>
-                        :
+                    rating > 0 ?
                         <div style={{position: 'absolute', top: '712px'}} onClick={() => {
-                            navigate("/map");
+                            navigate("/");
                         }}>
                             <ActivatedFeedback/>
+                        </div>
+                        :
+                        <div style={{position: 'absolute', top: '712px'}}>
+                            <DeactivatedFeedback/>
                         </div>
                 }
             </div>
         </div>
-    );
+    )
+        ;
 };
 
 export default EndCourse;
